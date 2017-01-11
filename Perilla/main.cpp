76a020fd0866ebf372548cba870a57d35cedf193
ms <ms@gavin.local>
2017-01-10 23:54:04 -0800

    Initial Commit

---
+++ Perilla/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Perilla
+//
+//  Created by ms on 1/10/17.
+//  Copyright © 2017 Gavin Mao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
